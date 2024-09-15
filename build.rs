47f47d32121b717c08cdb847d6bb218afd06b427
zxcv05 <g.uwu@tutanota.com>
2024-09-15 15:20:33 -0400

    Add protobuf-src to dependencies & bump version

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // No clue if this is good practice
+    // But hopefully it makes UX better?
+    // Mostly adding this because of github actions
+    std::env::set_var("PROTOC", protobuf_src::protoc());
+
     prost_build::compile_protos(&[
         "src/types/vendor/PreloadedUserSettings.proto",
         "src/types/vendor/FrecencyUserSettings.proto",
